85a9b9448603dd5b85993e70ecffb2a720465022
Laurentiu Molnar <lau.molnar2013@gmail.com>
2023-12-01 13:45:15 +0200

    Day 1

--- solutions/day1.go
+++ solutions/day1.go
@@ -2,11 +2,95 @@ package solutions
 
 import (
 	utils "aoc-2023/lib"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
-const sample = "ADD SAMPLE INPUT HERE"
+const sample = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+eightwo
+`
 const day = 1
 
-func Day1() {
+var numStringsMap = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+var numStrings = []string{
+	"one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7", "eight", "8", "nine", "9",
+}
+
+func Day1Part1() {
+	regex, _ := regexp.Compile(`(\d)`)
 	input := utils.GetInputOrSample(day, sample)
+	lines := strings.Split(input, "\n")
+	lines = lines[0 : len(lines)-1]
+
+	var sum int = 0
+
+	for _, line := range lines {
+		found := regex.FindAllString(line, -1)
+		s := found[0] + found[len(found)-1]
+
+		num, _ := strconv.Atoi(s)
+		sum += num
+	}
+	fmt.Println(sum)
+}
+
+func Day1Part2() {
+	input := utils.GetInputOrSample(day, sample)
+	lines := strings.Split(input, "\n")
+	lines = lines[0 : len(lines)-1]
+
+	var sum int = 0
+
+	for _, line := range lines {
+		firstIndex := len(line)
+		first := ""
+		lastIndex := -1
+		last := ""
+		for _, numStr := range numStrings {
+			fi := strings.Index(line, numStr)
+			if fi < firstIndex && fi >= 0 {
+				firstIndex = fi
+				first = numStr
+			}
+
+			li := strings.LastIndex(line, numStr)
+			if li > lastIndex {
+				lastIndex = li
+				last = numStr
+			}
+		}
+
+		firstNum, err := strconv.Atoi(first)
+		if err != nil {
+			firstNum = numStringsMap[first]
+		}
+
+		lastNum, err := strconv.Atoi(last)
+		if err != nil {
+			lastNum = numStringsMap[last]
+		}
+
+		num := 10*firstNum + lastNum
+		sum += num
+	}
+	fmt.Println(sum)
 }
