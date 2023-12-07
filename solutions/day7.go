9d0d488ba89f25d4045099e2d0f4ac42017b852c
Laurentiu Molnar <lau.molnar2013@gmail.com>
2023-12-08 01:10:34 +0200

    Day 7

--- solutions/day7.go
+++ solutions/day7.go
@@ -8,6 +8,16 @@ import (
 	"strings"
 )
 
+const (
+	highCard uint = iota
+	onePair
+	twoPairs
+	threeOfAKind
+	fullHouse
+	fourOfAKind
+	fiveOfAKind
+)
+
 type Hand struct {
 	cards  string
 	sorted string
@@ -86,47 +96,90 @@ func IsTwoPair(hand string) bool {
 	return ok
 }
 
-func computeScore(hand string) uint {
+func computeScoreWithJokers(hand string) uint {
 	pair, _ := IsPair(hand)
-	switch true {
-	case IsFive(hand):
-		return 6
-	case IsFour(hand):
-		return 5
-	case IsFull(hand):
-		return 4
-	case IsThree(hand):
-		return 3
-	case IsTwoPair(hand):
-		return 2
-	case pair:
-		return 1
-	default:
-		return 0
+	jokers := countJokers(hand)
+
+	if IsFive(hand) {
+		return fiveOfAKind
 	}
+
+	if IsFour(hand) {
+		if jokers == 1 || jokers == 4 {
+			return fiveOfAKind
+		}
+		return fourOfAKind
+	}
+
+	if IsFull(hand) {
+		if jokers == 2 || jokers == 3 { // only possible numbers of jokers in a full
+			return fiveOfAKind // if we have a full containing jokers, it's the same as having five identical cards
+		}
+
+		return fullHouse
+	}
+
+	if IsThree(hand) {
+		if jokers == 1 || jokers == 3 {
+			// 1 joker + three of a kind => four of a kind
+			return fourOfAKind
+		}
+
+		// 2 jokers + three of a kind is a full-house, so it's handled by previous if
+		return threeOfAKind
+	}
+
+	if IsTwoPair(hand) {
+		if jokers == 1 { // two pairs + 1 joker => full-house
+			return fullHouse
+		}
+
+		if jokers == 2 {
+			// 1 pair + 1 pair of jokers => four of a kind
+			return fourOfAKind
+		}
+
+		return twoPairs
+	}
+
+	if pair {
+		if jokers == 1 || jokers == 2 {
+			return threeOfAKind
+		}
+
+		// 3 jokers + 1 pair means full-house
+		return onePair
+	}
+
+	// the hand is high-card (no special combinations)
+	if jokers == 1 {
+		return onePair
+	}
+	// other numbers of jokers without other special combinations result in pairs or three/four/five of a kind, which are handled already
+
+	return highCard
 }
 
-func Day7Part1() {
-	sample := `32T3K 765
-T55J5 684
-KK677 28
-KTJJT 220
-QQQJA 483`
-	input := utils.GetInputOrSampleLines(7, sample)
+func buildHands(input []string, scoringFunc func(hand string) uint) *[]Hand {
 	handCount := len(input)
-
 	var hands []Hand = make([]Hand, handCount)
 	var bidVal uint64
 	var cards []string = make([]string, 5)
 	var sorted string
-	var total uint = 0
+
 	for i, line := range input {
 		split := strings.Split(line, " ")
 		bidVal, _ = strconv.ParseUint(split[1], 10, 0)
 		cards = strings.Split(split[0], "")
 		sort.Strings(cards)
 		sorted = strings.Join(cards, "")
-		hands[i] = Hand{cards: split[0], bid: uint(bidVal), sorted: sorted, index: i, value: computeScore(sorted)}
+		hands[i] = Hand{
+			cards:  split[0],
+			bid:    uint(bidVal),
+			sorted: sorted,
+			index:  i,
+			value:  scoringFunc(sorted),
+		}
 	}
 
 	sort.SliceStable(hands, func(i, j int) bool {
@@ -139,9 +192,65 @@ QQQJA 483`
 		}
 		return hands[i].value > hands[j].value
 	})
+	return &hands
+}
 
+func countJokers(hand string) uint {
+	return uint(strings.Count(hand, "J"))
+}
+
+func computeScore(hand string) uint {
+	pair, _ := IsPair(hand)
+	switch true {
+	case IsFive(hand):
+		return fiveOfAKind
+	case IsFour(hand):
+		return fourOfAKind
+	case IsFull(hand):
+		return fullHouse
+	case IsThree(hand):
+		return threeOfAKind
+	case IsTwoPair(hand):
+		return twoPairs
+	case pair:
+		return onePair
+	default:
+		return highCard
+	}
+}
+
+func Day7Part1() {
+	sample := `32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483`
+	input := utils.GetInputOrSampleLines(7, sample)
+	hands := buildHands(input, computeScore)
+	handCount := len(input)
+
+	var total uint
 	for i := 0; i < handCount; i++ {
-		total += hands[i].bid * (uint(handCount - i))
+		total += (*hands)[i].bid * (uint(handCount - i))
+	}
+	fmt.Println("Total:", total)
+}
+
+func Day7Part2() {
+	sample := `32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483`
+	input := utils.GetInputOrSampleLines(7, sample)
+	cardValues['J'] = 0 // we won't run part1 after part2, so it's no problem reassigning J's value in part 2
+
+	hands := buildHands(input, computeScoreWithJokers)
+	handCount := len(input)
+
+	var total uint
+	for i := 0; i < handCount; i++ {
+		total += (*hands)[i].bid * (uint(handCount - i))
 	}
 	fmt.Println("Total:", total)
 }
